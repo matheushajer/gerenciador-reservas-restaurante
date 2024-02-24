ee559893864cff7d6f7351dc7079d71047e709cb
matheushajer <maatheushajer@gmail.com>
2024-02-24 08:25:40 -0300

    Criação do projeto

---
+++ src/main/java/br/com/fiap/gerenciadorDeReservas/GerenciadorDeReservasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gerenciadorDeReservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeReservasApplication.class, args);
+	}
+
+}
